75547cefa3c4ec0211187bdbd36942b12402284f
ZekiiYF <eggarcia@fi.uba.ar>
2026-04-24 19:44:24 -0300

    Archivos de constantes y dataset agregados a la nueva modularizacion

---
+++ nueva_modularizacion/constantes.py
@@ -0,0 +1,89 @@
+"""Constantes globales para FundaDT."""
+
+# ----- mensajes generales -----
+MSG_FIN = "Finalizando..."
+
+# ----- templates generales -----
+HEADER_EQUIPOS = "Equipos disponibles:"
+HEADER_JUGADORES = "Jugadores disponibles:"
+TEMPLATE_EQUIPO = "{n}. {nombre_equipo}"
+
+# ----- errores generales -----
+ERROR_SELECCION_INVALIDA = "Selección inválida"
+ERROR_INPUT_INVALIDO = "Formato de entrada inválido"
+ERROR_SIN_EQUIPOS = "No existen equipos registrados"
+
+# ----- crear equipo -----
+MSG_EQUIPO_CREADO = (
+    "El equipo {nombre_equipo} ha sido creado exitosamente con un presupuesto de ${presupuesto}"
+)
+ERROR_EQUIPO_EXISTENTE = "Ya existe un equipo con ese nombre"
+ERROR_FORMATO_NOMBRE = "Formato de nombre de equipo inválido"
+
+# ----- comprar jugador -----
+MSG_COMPRA_EXITOSA = (
+    "El jugador {nombre_jugador} fue comprado para el equipo {nombre_equipo}. "
+    "El nuevo presupuesto del equipo es: ${presupuesto}"
+)
+TEMPLATE_PRESUPUESTO = "El presupuesto del equipo es ${presupuesto}"
+TEMPLATE_JUGADOR_PRECIO = "{n}. {nombre_jugador} - ${precio}"
+ERROR_PRESUPUESTO_INSUFICIENTE = "El presupuesto es insuficiente"
+ERROR_JUGADOR_EXISTENTE = "El jugador {nombre_jugador} ya se encuentra en el plantel"
+
+# ----- vender jugador -----
+TEMPLATE_PLANTEL = """Equipo: {nombre_equipo}
+Presupuesto: ${presupuesto}
+Plantel:
+"""
+TEMPLATE_JUGADOR_PLANTEL = "{n}. {nombre_jugador} ({posicion}) [{rol_actual}] - ${precio}"
+MSG_PLANTEL_VACIO = "El equipo no posee jugadores"
+MSG_VENTA_EXITOSA = (
+    "El jugador {nombre_jugador} fue vendido exitosamente. "
+    "El nuevo presupuesto del equipo es: ${presupuesto}"
+)
+MSG_ALINEACION_DESARMADA = "Atención: La alineación del equipo ha sido desarmada"
+
+# ----- ver plantel -----
+# usa las mismas que vender jugador
+
+# ----- armar alineación -----
+MSG_ALINEACION_GUARDADA = "La nueva alineación del equipo {nombre_equipo} fue guardada exitosamente"
+HEADER_ARQUERO = "> ARQUERO:"
+HEADER_DEFENSORES = "> DEFENSORES:"
+HEADER_MEDIOCAMPISTAS = "> MEDIOCAMPISTAS:"
+HEADER_DELANTEROS = "> DELANTEROS:"
+HEADER_SUPLENTES = "> SUPLENTES:"
+HEADER_TITULARES = "Los jugadores titulares son:"
+TEMPLATE_JUGADOR = "{n}. {nombre_jugador}"
+TEMPLATE_JUGADOR_POSICION = "{n}. {nombre_jugador} - [{posicion}]"
+ERROR_FORMACION_INVALIDA = "Formación inválida"
+MSG_CANTIDAD_JUGADORES_INSUFICIENTE = (
+    "No hay suficientes jugadores disponibles para armar esta alineación"
+)
+ERROR_CANTIDAD_INVALIDA = "Para esta posición se necesita(n) {cantidad} jugador(es)"
+
+# ----- ver alineación -----
+TEMPLATE_ALINEACION = """La formación es: {formacion}
+Los jugadores titulares son:
+Delanteros: {delanteros}
+Mediocampistas: {mediocampistas}
+Defensores: {defensores}
+Arquero: {arquero}
+Capitán: {capitan}
+Los suplentes son:
+{suplentes}
+"""
+MSG_ALINEACION_INEXISTENTE = "El equipo no posee una alineación definida"
+
+# ----- ver jugador más utilizado -----
+TEMPLATE_ESTADISTICA_JUGADORES = """Arquero: {arquero}
+Defensor: {defensor}
+Mediocampista: {mediocampista}
+Delantero: {delantero}
+"""
+MSG_NO_INFORMACION = "No hay información suficiente para mostrar estadísticas"
+
+# ----- constantes de retorno -----
+SALIR = "salir"
+PEDIR_DE_NUEVO = "pedir de nuevo"
+ENTRADA_SALIR = "**"
